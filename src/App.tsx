1a14b1ec0f0b3f79514d9d0db12581d78261ee78
iniken <kiparis994@gmail.com>
2020-08-16 22:15:17 +0700

    3) Add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,34 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Raiting} from "./components/Raiting/Raiting";
 
 function App() {
-    debugger;
     return (
         <div>
+            <PageTitle title={"Hello"}/>
+            <PageTitle title={"Yo"}/>
+            Article 1
+            <Raiting value={3}/>
+            <Accordion title={"Main Cool MENU"}/>
+            <Accordion title={"Luxary MENU"}/>
 
-            <AppTitle />
-            <Raiting />
-            <Accordion />
-            <Raiting />
+            Article 2
+            <Raiting value={0}/>
+            <Raiting value={1}/>
+            <Raiting value={2}/>
+            <Raiting value={3}/>
+            <Raiting value={4}/>
+            <Raiting value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
+function PageTitle(props: any) {
     debugger;
     return (
-    <>This is App component</>
+        <h1>{props.title}</h1>
     )
-}
-
-function Raiting() {
-    debugger;
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
-}
-
-function Star() {
-    debugger;
-    return (
-        <div>Star</div>
-    );
-}
-
-function Accordion() {
-    debugger;
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    );
-}
-
-
-function AccordionTitle() {
-    debugger;
-    return (
-            <h3>Меню</h3>
-    );
-}
-
-
-function AccordionBody() {
-    debugger;
-    return (
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-    );
 }
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Accordion(props: any) {
+    return (
+        <div>
+            <AccordionTitle title={props.title}/>
+            <AccordionBody/>
+        </div>
+    );
+}
+
+
+function AccordionTitle(props:any) {
+    debugger;
+    return (
+        <h3>{props.title}</h3>
+    );
+}
+
+
+function AccordionBody() {
+    debugger;
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+export default Accordion;
