404567655f15eee3aef00b39038778b58b149166
iniken <kiparis994@gmail.com>
2020-09-09 21:39:51 -1000

    6) useState home task (part 11)

--- src/components/UncontrolledAccordion/Accordion.tsx
+++ src/components/UncontrolledAccordion/Accordion.tsx
@@ -52,3 +52,4 @@ export default UncontrolledAccordion;
 export default UncontrolledAccordion;
 export default UncontrolledAccordion;
 export default UncontrolledAccordion;
+export default UncontrolledAccordion;
